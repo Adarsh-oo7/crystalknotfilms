f4eba04f012f7ddd626ff13515062054c4926def
Adarsh-oo7 <adarshsarachandran@gmail.com>
2025-08-04 00:17:05 +0530

    adsf

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -39,7 +39,7 @@ export function Hero() {
         <Header/>
       </div>
       <video autoPlay muted loop playsInline className="absolute inset-0 w-full h-full object-cover">
-        <source src="./videos/intro.mp4" type="video/mp4" />
+        <source src="/videos/intro.mp4" type="video/mp4" />
       </video>
       <div className="absolute inset-0 bg-black/80 " />
       <div className="relative z-20 flex flex-1 items-center justify-center px-6 lg:px-8 min-h-screen">
@@ -51,7 +51,7 @@ export function Hero() {
             className="mb-8"
           >
             <Image
-              src="./images/Ck logo.png"
+              src="/images/Ck logo.png"
               alt="Company Logo"
               width={150}
               height={150}
