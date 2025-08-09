fb19d47a317d63b79e621c48056077ce54d10046
Aromalvg <aromalvijayan448@gmail.com>
2025-08-09 22:05:46 +0530

    nav

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -92,7 +92,7 @@ export const NavBody = ({ children, className, visible }: NavBodyProps) => {
       }}
       transition={{ type: "spring", stiffness: 200, damping: 50 }}
       className={cn(
-        "relative z-[60] mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-full  px-4 py-2 lg:flex bg-black ",
+        "relative z-[60] mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-full  px-4 py-2 lg:flex bg-black/40 ",
         visible && "bg-black/40 dark:bg-neutral-950/80",
         className
       )}
@@ -150,14 +150,14 @@ export const MobileNav = ({ children, className, visible }: MobileNavProps) => (
         ? "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset"
         : "none",
       width: visible ? "90%" : "100%",
-      paddingRight: visible ? "12px" : "0px",
-      paddingLeft: visible ? "12px" : "0px",
+      paddingRight: visible ? "12px" : "6px",
+      paddingLeft: visible ? "12px" : "6px",
       borderRadius: visible ? "4px" : "2rem",
       y: visible ? 20 : 0,
     }}
     transition={{ type: "spring", stiffness: 200, damping: 50 }}
     className={cn(
-      "relative z-50 mx-auto flex w-full max-w-[calc(100vw-2rem)] flex-col items-center justify-between bg-transparent px-0 py-2 lg:hidden",
+      "relative z-50 mx-auto flex w-full max-w-[calc(100vw-2rem)] flex-col items-center justify-between bg-black/40 px-0 py-2 lg:hidden",
       visible && "bg-black/40 dark:bg-black/40",
       className
     )}
